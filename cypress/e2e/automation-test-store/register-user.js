f98f5921ec05f5694a61c19e834466afe9ba2487
Lena Brd <elenarosso11@gmail.com>
2024-08-23 08:18:36 +0300

    added random username and email

--- cypress/e2e/automation-test-store/register-user.js
+++ cypress/e2e/automation-test-store/register-user.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
 
 describe('Login or Register', () => {
+   let loginName = 'username' + Math.random().toString(36).substring(2);
+   let email = 'email' + Math.random().toString(15).substring(2) + '@example.com'
+
+
    before(() => {
       cy.fixture('user-credentials').then((data) => {
          globalThis.data = data
@@ -20,7 +24,7 @@ describe('Login or Register', () => {
 
       cy.get('#AccountFrm_firstname').type(data.firstName);
       cy.get('#AccountFrm_lastname').type(data.lastName);
-      cy.get('#AccountFrm_email').type(data.email);
+      cy.get('#AccountFrm_email').type(email);
       cy.get('#AccountFrm_telephone').type(data.telephone);
       cy.get('#AccountFrm_address_1').type(data.address);
       cy.get('#AccountFrm_city').type(data.city);
@@ -28,7 +32,7 @@ describe('Login or Register', () => {
       cy.get('#AccountFrm_zone_id').select('North Yorkshire');
       cy.get('#AccountFrm_postcode').type(125878);
 
-      cy.get('#AccountFrm_loginname').type(data.loginName);
+      cy.get('#AccountFrm_loginname').type(loginName);
       cy.get('#AccountFrm_password').type(data.password);
       cy.get('#AccountFrm_confirm').type(data.password);
 
@@ -39,7 +43,13 @@ describe('Login or Register', () => {
       cy.get('[title="Continue"]').click();
 
       cy.get('.maintext').should('contain', ' Your Account Has Been Created!')
-
-      
    });
+
+   it('User can login to his account', () => {
+      cy.get('#customer_menu_top').click();
+      cy.get('.returncustomer > .heading2').should('contain', 'Returning Customer');
+      cy.get('#loginFrm_loginname').type(loginName);
+      cy.get('#loginFrm_password').type(data.password);
+      cy.get('[title="Login"]').click();
+   })
 })
